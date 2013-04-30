a6b6fe53cd1c269faefdcd2ef5d74e89e6829fd1
Jürgen Geuter <tante@tante.cc>
2013-04-30 16:13:07 +0200

    Bump

--- share.py
+++ share.py
@@ -7,8 +7,7 @@ Licensed under the Eiffel Forum License 2.
 """
 
 import tweepy
-import time
-import re
+import urllib
 
 def configure(config):
     """This module takes its Twitter configuration from the twitter module.
@@ -22,10 +21,6 @@ def configure(config):
         config.interactive_add('share', 'identica_pass', 'identica_pass')
         pass
 
-def format_thousands(integer):
-    """Returns string of integer, with thousands separated by ','"""
-    return re.sub(r'(\d{3})(?=\d)', r'\1,', str(integer)[::-1])[::-1]
-
 def f_update(willie, trigger):
     """Share with Willie's Twitter/identi.ca/ADN account. Admin-only."""
     if trigger.admin:
@@ -34,19 +29,37 @@ def f_update(willie, trigger):
         auth.set_access_token(willie.config.twitter.access_token, willie.config.twitter.access_token_secret)
         api = tweepy.API(auth)
         # identi.ca needs no api
-
+        passwd_manager = urllib.request.HTTPPasswordMgr()
+        passwd_manager.add_password("Identi.ca API", "http://identi.ca/api/statuses", willie.config.share.identica_user, willie.config.share.identica_pass)
+        auth_handler = urllib.request.HTTPBasicAuthHandler(passwd_manager)
+        page_opener = urllib.request.build_opener(auth_handler)
+        urllib.request.install_opener(page_opener)
+        # ADN
+        app = adn.ADN(access_token=willie.config.share.adn_access_token)
         update = str(trigger.group(2))
 
         if len(update) <= 140:
+            #twitter
             api.update_status(update)
-            willie.reply("Successfully posted to my twitter account.")
+            #ADN (which throws an exception just for fun)
+            try:
+                app.createPost(text=update)
+            except:
+                pass
+            #identi.ca
+            parms = urllib.parse.urlencode({'status': update})
+            parms = parms.encode("utf8")
+            resp = urllib.request.urlopen("http://identi.ca/api/statuses/update.xml", parms)
+            resp.read().decode("utf8")
+            
+            willie.reply("Successfully posted to my Social Media  accounts.")
         else:
             toofar = len(update) - 140
             willie.reply("Please shorten the length of your message by: " + str(toofar) + " characters. To post to Twitter and Identi.ca")
 
-f_update.commands = ['share']
+f_update.commands = ['post']
 f_update.priority = 'medium'
-f_update.example = '.share Hello World!'
+f_update.example = '.post Hello World!'
 
 if __name__ == '__main__':
     print __doc__.strip()

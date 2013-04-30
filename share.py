9376742ca94597a69bb3a2794aefa4373f65717c
Jürgen Geuter <tante@tante.cc>
2013-04-30 16:20:19 +0200

    Buggy crap is buggy

--- share.py
+++ share.py
@@ -8,6 +8,7 @@ Licensed under the Eiffel Forum License 2.
 
 import tweepy
 import urllib
+import adn
 
 def configure(config):
     """This module takes its Twitter configuration from the twitter module.
@@ -21,7 +22,7 @@ def configure(config):
         config.interactive_add('share', 'identica_pass', 'identica_pass')
         pass
 
-def f_update(willie, trigger):
+def post_update(willie, trigger):
     """Share with Willie's Twitter/identi.ca/ADN account. Admin-only."""
     if trigger.admin:
         # twitter API
@@ -29,13 +30,9 @@ def f_update(willie, trigger):
         auth.set_access_token(willie.config.twitter.access_token, willie.config.twitter.access_token_secret)
         api = tweepy.API(auth)
         # identi.ca needs no api
-        passwd_manager = urllib.request.HTTPPasswordMgr()
-        passwd_manager.add_password("Identi.ca API", "http://identi.ca/api/statuses", willie.config.share.identica_user, willie.config.share.identica_pass)
-        auth_handler = urllib.request.HTTPBasicAuthHandler(passwd_manager)
-        page_opener = urllib.request.build_opener(auth_handler)
-        urllib.request.install_opener(page_opener)
+        # and is down atm
         # ADN
-        app = adn.ADN(access_token=willie.config.share.adn_access_token)
+        app = adn.Adn(access_token=willie.config.share.adn_access_token)
         update = str(trigger.group(2))
 
         if len(update) <= 140:
@@ -47,19 +44,15 @@ def f_update(willie, trigger):
             except:
                 pass
             #identi.ca
-            parms = urllib.parse.urlencode({'status': update})
-            parms = parms.encode("utf8")
-            resp = urllib.request.urlopen("http://identi.ca/api/statuses/update.xml", parms)
-            resp.read().decode("utf8")
             
             willie.reply("Successfully posted to my Social Media  accounts.")
         else:
             toofar = len(update) - 140
             willie.reply("Please shorten the length of your message by: " + str(toofar) + " characters. To post to Twitter and Identi.ca")
 
-f_update.commands = ['post']
-f_update.priority = 'medium'
-f_update.example = '.post Hello World!'
+post_update.commands = ['post']
+post_update.priority = 'medium'
+post_update.example = '.post Hello World!'
 
 if __name__ == '__main__':
     print __doc__.strip()
